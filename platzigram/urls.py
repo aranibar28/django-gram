d58b9cfd03e9bb8c6b63cbe1fc3a89d128cc003f
Gerson Aranibar <aranibargerson28@gmail.com>
2021-10-31 19:49:15 -0500

    update urls and views

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import hello_world, hi
+from .views import hello_world, sorted_integer, say_hi
 
 urlpatterns = [
-    path('hello-world/', hello_world),
-    path('hi/', hi)
+    # /hello-world/
+    path('hello-world/', hello_world), 
+    # /num/?numbers=10,4,50,32,25
+    path('num/', sorted_integer),
+    # /hi/Gerson/24/
+    path('hi/<str:name>/<int:age>/', say_hi)
 ]
--- platzigram/views.py
+++ platzigram/views.py
@@ -1,12 +1,29 @@
-# Django
 from django.http import HttpResponse
-# Utilities
 from datetime import datetime
+import json
 
 def hello_world(request):
     now = datetime.now().strftime('%b %dth %Y - %H:%M hrs')
     return HttpResponse('Hola papu, esta es la hora del servidor: {now}'.format(now=now))
 
-def hi(request):
-    # import pdb; pdb.set_trace()
-    return HttpResponse("Holaa !!!")
+def sorted_integer(request):
+    numbers = request.GET['numbers']
+    sorted_numbers = sorted(map(int, numbers.split(',')))
+    content_type='application/json'
+
+    data = {
+        'status': 'success',
+        'numbers': sorted_numbers,
+        'messages': 'Integers sorted successfully.'
+    }
+
+    return HttpResponse(json.dumps(data, indent=4), content_type)
+
+def say_hi(request, name, age):
+    
+    if age < 12:
+        message = 'Sorry {}, you are not allowed here'.format(name)
+    else:
+        message = 'Hello, {}! Welcome to Platzi'.format(name)
+
+    return HttpResponse(message)
