06b9d6b2d150142d48561a865a47fbab0757271a
Gerson Aranibar <aranibargerson28@gmail.com>
2021-10-31 22:52:12 -0500

    lista post

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import hello_world, sorted_integer, say_hi
+from posts.views import list_posts
 
 urlpatterns = [
     # /hello-world/
@@ -7,5 +8,7 @@ urlpatterns = [
     # /num/?numbers=10,4,50,32,25
     path('num/', sorted_integer),
     # /hi/Gerson/24/
-    path('hi/<str:name>/<int:age>/', say_hi)
+    path('hi/<str:name>/<int:age>/', say_hi),
+
+    path('posts/', list_posts)
 ]
